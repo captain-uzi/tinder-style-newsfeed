8588dda980993e22fd181d409373d34436478f50
uzair <32576120+captain-uzi@users.noreply.github.com>
2022-01-13 22:38:06 +0530

    Add files via upload

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Rename this file to "firebaseConfig.js" before use
+// Replace all XXXXs with real Firebase API keys
+
+export default {
+  apiKey: "AIzaSyDU-ftjO7Y0PHHmR7UMeBsbCdo8nYopl6A",
+  authDomain: "uns-app-testing.firebaseapp.com",
+  databaseURL: "https://uns-app-testing-default-rtdb.firebaseio.com",
+  projectId: "uns-app-testing",
+  storageBucket: "uns-app-testing.appspot.com",
+  messagingSenderId: "186641919701",
+  appId: "1:186641919701:web:5d5db61bc68fad6f51bc5c",
+  measurementId: "G-266LNVX4JN",
+};
